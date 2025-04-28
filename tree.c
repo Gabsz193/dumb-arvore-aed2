768cf4590d0d1be201942a08639e9e05eb2b1326
Luiz Gabriel <senaaluizgabriel@gmail.com>
2025-04-28 12:08:45 -0400

    Função de remover incompleta

--- tree.c
+++ tree.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 #include "tree.h"
 
-
 Tree* initialize() {
     return NULL;
 };
 
-char is_empty(Tree* t) {
+char is_empty(Tree *t) {
     return t == NULL;
 }
 
@@ -69,8 +68,59 @@ void walk_tree_post_order(Tree *t, void (*func)(int v)) {
     func(t->v);
 }
 
-Tree* remove_value(Tree* t, int v) {
-    if(is_empty(t)) {
+char has_only_child(Tree *t) {
+    return (!t->l && t->r) || (t->l && !t->r);
+}
 
+char no_child(Tree *t) {
+    return !t->r && !t->l; 
+}
+
+void foo(Tree *t, Tree *b, Tree *s) {
+    if(b->r == t) {
+        b->r = s;
+    } else {
+        b->l = s;
     }
+    free(t);
+}
+
+char _remove_value(Tree *t, Tree *b, int v) {
+    if(is_empty(t)) {
+        return 0;
+    }
+    if(t->v == v) {
+        if(has_only_child(t) || no_child(t)) {
+            if(t->l) {
+                foo(t, b, t->l);
+            }
+            if(t->r) {
+                foo(t, b, t->r);
+            }
+        } else {
+            Tree *tmp1 = t->l, *tmp2;
+
+            while(tmp1) {
+                tmp2 = tmp1;
+                tmp1 = tmp1->r;               
+            }
+
+            
+
+            tmp2->l = t->l;
+            tmp2->r = t->r;
+
+            foo(t, b, tmp2);
+            
+        }
+    }
+    if(t->v >= v) {
+        _remove_value(t->l, t, v);
+    } else {
+        _remove_value(t->r, t, v);
+    }
+}
+
+char remove_value(Tree *t, int v) {
+    _remove_value(t, NULL, v);
 }
